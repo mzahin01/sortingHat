f5d8d0e7f460d5f89177abf5283694772dd8362f
mzahin01 <minhazahin@gmail.com>
2024-09-14 18:39:51 +0600

    Splash working

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,22 +1,26 @@
 // ignore_for_file: constant_identifier_names
-
 import 'package:get/get.dart';
 
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/splash_page/ui/splash_page.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.SPLASH_PAGE;
 
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH_PAGE,
+      page: () => const SplashPage(),
     ),
   ];
 }
